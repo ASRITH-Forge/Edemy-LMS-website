8816373bf04aa8ffa8a9b525694face688814004
asrith-raju <asrithraju143@gmail.com>
2026-02-16 10:30:00 +0530

    client(docs): add inline comments for educator Page

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,23 +1,37 @@
+// Importing React, routing outlet, and layout components
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/educator/Navbar'
 import SideBar from '../../components/educator/SideBar'
 import Footer from '../../components/educator/Footer'
 
+// Educator layout component (wrapper for educator pages)
 const Educator = () => {
   return (
+    // Main layout container
     <div className='text-default min-h-screen bg-white'>
+
+      {/* Top navigation */}
       <Navbar/>
+
+      {/* Sidebar + page content */}
       <div className='flex'> 
+
+        {/* Sidebar navigation */}
         <SideBar/>
+
+        {/* Dynamic routed content */}
         <div className='flex-1'>
-        {<Outlet/>} 
+          {<Outlet/>} 
         </div>
-       
-        </div>
-         <Footer/>
+
+      </div>
+
+      {/* Footer */}
+      <Footer/>
     </div>
   )
 }
 
+// Exporting component
 export default Educator
