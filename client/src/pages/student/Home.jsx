7e7de87e8d7423d45ee61fab9b3f1b06eb3645e8
asrith-raju <asrithraju143@gmail.com>
2026-02-18 10:30:00 +0530

    client(docs): add inline comments for student Home page

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,3 +1,4 @@
+// Importing homepage sections/components
 import React from 'react'
 import Hero from '../../components/student/Hero'
 import Companies from '../../components/student/Companies'
@@ -6,17 +7,32 @@ import Testimonials from '../../components/student/Testimonials'
 import CallToAction from '../../components/student/CallToAction'
 import Footer from '../../components/student/Footer'
 
+// Home component (main landing page)
 const Home = () => {
   return (
+    // Layout container
     <div className='flex flex-col items-center space-y-7 text-center'>
+
+      {/* Hero section */}
       <Hero/>
+
+      {/* Partner companies */}
       <Companies/>
+
+      {/* Courses preview */}
       <CoursesSection/>
+
+      {/* User testimonials */}
       <Testimonials/>
+
+      {/* CTA section */}
       <CallToAction/>
+
+      {/* Footer */}
       <Footer/>
     </div>
   )
 }
 
+// Exporting component
 export default Home
