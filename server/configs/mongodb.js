a60238d61b14d77d3d3789620ac598b1db54eb92
asrith-raju <asrithraju143@gmail.com>
2026-02-19 10:30:00 +0530

    server(docs): add inline comments for mongodb file

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,12 +1,17 @@
+// Importing mongoose
 import mongoose from "mongoose";
 
-//connect to the MongoDB database
-
+// Connect to MongoDB database
 const  connectDB = async () => {
+
+    // Log when DB connection is successful
     mongoose.connection.on('connected',()=>{
         console.log('Database Connected');
     })
+
+    // Establish connection using environment URI
     await mongoose.connect(`${process.env.MONGODB_URI}/Edemy`)
 }
 
-export default connectDB; 
+// Exporting function
+export default connectDB;
