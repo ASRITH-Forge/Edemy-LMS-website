71fd132ac5a74283d75a0301aea23d67c0acde7b
asrith-raju <asrithraju143@gmail.com>
2026-02-19 10:30:00 +0530

    server(docs): add inline comments for cloudinary configuration file

--- server/configs/cloudinary.js
+++ server/configs/cloudinary.js
@@ -1,6 +1,10 @@
+// Importing Cloudinary SDK
 import {v2 as cloudinary} from 'cloudinary'
 
+// Function to configure Cloudinary connection
 const connectCloudinary = async() =>{
+
+    // Setting Cloudinary credentials from environment variables
     cloudinary.config({
         cloud_name: process.env.CLOUDINARY_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
@@ -9,4 +13,5 @@ const connectCloudinary = async() =>{
 
 }
 
+// Exporting function
 export default connectCloudinary;
